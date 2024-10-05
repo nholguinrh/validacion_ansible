2f38d5c535d4f9bf055c6776886184978e72cf91
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2024-10-05 02:48:54 -0500

    Update revisarReto.sh

--- revisarReto.sh
+++ revisarReto.sh
@@ -1,9 +1,9 @@
 cd
 sudo yum -y install ansible-core git
-rm -fr /var/log/ansible_validations.log
+sudo rm -fr /var/log/ansible_validations.log
 git clone https://github.com/nholguinrh/validacion_ansible.git
 cd validacion_ansible
 git pull
-ansible-playbook prueba-todo.yaml
+sudo ansible-playbook prueba-todo.yaml
 cd
 cat /var/log/ansible_validations.log
